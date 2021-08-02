b15c8c33b2bc288a2dfc35c07daf1c8a4224741d
Brandon Fortes <bman2505@yahoo.com>
2021-08-02 13:58:40 -0500

    controller mapped.

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,12 +1,16 @@
 package frc.robot;
 
 public class RobotMap {
+
+    //Controllers
+    
+      public final static int DRIVER_JOYSTICK_1_USB_ID = 0; 
     
     // Drive
-    public static int leftFrontMotor = 0;
-    public static int leftRearMotor = 1; 
-    public static int rightFrontMotor = 2;
-    public static int rightRearMotor = 3; 
+      public static int leftFrontMotor = 3;
+      public static int leftRearMotor = 2; 
+      public static int rightFrontMotor = 0;
+      public static int rightRearMotor = 1; 
 
     
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
 import frc.robot.RobotMap;
